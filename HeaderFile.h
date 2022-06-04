1edcf9e8cf8ba21bb2ebc8c7fdd5796762db6527
Sheharyar <sheharyarone@gmail.com>
2022-06-04 15:45:36 +0500

    Initial commit

---
+++ HeaderFile.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person {
+
+    int age;
+    string name;
+    string address;
+    string phone;
+    string email;
+    
+
+};
