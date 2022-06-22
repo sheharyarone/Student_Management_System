7fc56f97acf5bc9469fc9602bf43d2e4b7ec3c13
Sheharyar <sheharyarone@gmail.com>
2022-06-22 23:49:18 +0500

    ADDING BLUEPRINT OF BASIC FUNCTIONALITY AND SOME FUNCTIONS RELATED TO BASE CLASS

--- HeaderFile.h
+++ HeaderFile.h
@@ -4,11 +4,51 @@ using namespace std;
 
 class Person {
 
-    int age;
+    public :
+
+    int user_id;
+    string password_check;
     string name;
-    string address;
-    string phone;
-    string email;
-    
+    string password ;
+    string title;
+    bool isActive;
+    int userLevel;
+
+    public:
+
+    void Login();
+    void Logout();
+    void initPassword();
+    void changePassword();
+    void ListAllUsers();
+    void AddNewTutor();
+    void ModifyTutor();
+    void AddNewSubject();
+    void ModifySubject();
+    void ShowStudents();
+    void ShowTutors();
+    void ShowSubjects();
+    void CheckUserLevel();
+    void resetUserPassword();
+    void lockOrUnlockUser();
+
+};
+class Student : public Person 
+{
+    public :
+    void requestJoinSubject();
+
+
+};
+class Tutor : public Person 
+{
+    public:
+    void addCommentOfUserRequest();
+
+};
+class Teacher : public Person 
+{
+    public:
+    void acceptOrDenyUserRequest();
 
 };
